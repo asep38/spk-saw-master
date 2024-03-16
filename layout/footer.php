a01967e3f0414232edad2a81b32d95433baf12ed
asep z <112145050+asep38@users.noreply.github.com>
2024-03-16 19:14:11 +0700

    hapus float-footer

--- layout/footer.php
+++ layout/footer.php
@@ -3,8 +3,6 @@
         <div class="float-start">
             <p>2024 &copy; SPK - SAW Penilaian Kinerja Guru di SMPN 1 Cihaurbeuti</p>
         </div>
-        <div class="float-end">
-            <p>Created by Puji Rahmi Nopitasari</p>
-        </div>
+        
     </div>
 </footer>
